23e5b3f795399bc5f8d5710a8edaf83d84397898
ejp314 <ejp314@uw.edu>
2017-05-21 22:13:33 -0700

    0: Start generator and change spacing on ApplicationMain.

--- src/main/scala/com/example/model/ApplicationMain.scala
+++ src/main/scala/com/example/model/ApplicationMain.scala
@@ -9,7 +9,6 @@ object ApplicationMain extends App {
   val syncSend = Event(EventId("1"), Partner("abc"), TimeStamp(""), Send, None, None)
 
   val asyncSend = Event(EventId("2"), Partner("abc"), TimeStamp(""), Send, Some(AckPending), None)
-
   val receiptReceive = Event(EventId("3"), Partner("abc"), TimeStamp(""), Receive, None, Some(asyncSend.id))
 
   println("sync send = [" + syncSend + "]")
---
+++ src/main/scala/com/example/model/Generator.scala
@@ -0,0 +1,8 @@
+package com.example.model
+
+/**
+  * Simple generator to create some events.
+  */
+object Generator {
+
+}
